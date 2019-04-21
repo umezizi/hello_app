3db0e050ba45f369c7d5271beccc09a04b322495
Umezizi <umeziro.v2@gmail.com>
2019-04-21 05:04:08 +0000

    演習２

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   #root 'application#hello'
-  root 'application#hola'
-  #root 'application#goodbye'
+  #root 'application#hola'
+  root 'application#goodbye'
 end
